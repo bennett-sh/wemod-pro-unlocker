8c6ee1fc02a4f1d3eb66b24801fbecfbb52270b6
Rexy <rexyrexy@users.noreply.github.com>
2022-12-22 03:07:02 +0300

    Update fetchIntercept.js

--- cli/src/fetchIntercept.js
+++ cli/src/fetchIntercept.js
@@ -2,7 +2,7 @@ if(new URL(e.url).pathname.endsWith('/account')) {
   return {
     ...JSON.parse(t),
     subscription: {
-      "type": "pro"
+      type: 'pro'
     },
     username: '🔓WeMod Pro Unlocker',
     profileImage: 'static/shared/images/default-profile-image.svg',
